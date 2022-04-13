a2992d1023dfb49f5aa030f73292d60d32a59537
Geralexan <g.lagos05@ufromail.cl>
2022-04-13 00:41:32 -0400

    2do Commit German

--- src/main/java/CalculadoraCientifica.java
+++ src/main/java/CalculadoraCientifica.java
@@ -13,27 +13,27 @@ public class CalculadoraCientifica {
     }
 
     public static double[] ecuacionCuadratica() {
-        double[] solucionesCuadratica = new double[1];
-        int a = 0;
-        int b = 0;
-        int c = 0;
+        double[] solucionesCuadratica = new double[2];
+        double a = 0;
+        double b = 0;
+        double c = 0;
         double discriminante = (Math.pow(b,2)-(4*a*c));
         double raizDiscriminante = Math.sqrt(discriminante);
-        double x1 = -((b + raizDiscriminante/2*a));
-        double x2 = -((b - raizDiscriminante/2*a));
+        double x1 = (-b+raizDiscriminante)/(2*a);
+        double x2 = (-b-raizDiscriminante)/(2*a);
         solucionesCuadratica[0] = x1;
         solucionesCuadratica[1] = x2;
         return solucionesCuadratica;
     }
 
     public static double[] sistemaEcuaciones() {
-        double[] solucionesSistema = new double[1];
-        int a = 0;
-        int b = 0;
-        int c = 0;
-        int d = 0;
-        int e = 0;
-        int f = 0;
+        double[] solucionesSistema = new double[2];
+        double a = 0;
+        double b = 0;
+        double c = 0;
+        double d = 0;
+        double e = 0;
+        double f = 0;
         double x,y;
         y = ((f * a) - (d * c)) / ((e * a) - (d * b));
         x = (c - (b * y)) / a;
@@ -43,21 +43,22 @@ public class CalculadoraCientifica {
     }
 
     public static double[] ecuacionRecta() {
-        double[] solucionRecta = new double[1];
-        int x1 = 0;
-        int x2 = 0;
-        int y1 = 0;
-        int y2 = 0;
-        double pendiente = (y1 - y2) / (x2 - x1);
-        double b = (pendiente*(-x1)+y1);
+        double[] solucionRecta = new double[2];
+        double x1 = 0;
+        double y1 = 0;
+        double x2 = 0;
+        double y2 = 0;
+        double pendiente = (y2 - y1) / (x2 - x1);
+        double b = ((-x1)*pendiente)+y1;
         solucionRecta[0] = pendiente;
         solucionRecta[1] = b;
         return solucionRecta;
     }
 
     public static void mostrarEcuacionCuadratica(double[] solucionesCuadratica) {
-        System.out.println("x1 es: "+solucionesCuadratica[0]);
-        System.out.println("x2 es: "+solucionesCuadratica[1]);
+        System.out.println("Las soluciones de la ecuación son: ");
+        System.out.println("x1 = "+solucionesCuadratica[0]);
+        System.out.println("x2 = "+solucionesCuadratica[1]);
     }
 
     public static void mostrarSistemaEcuaciones(double[] solucionesSistema) {
